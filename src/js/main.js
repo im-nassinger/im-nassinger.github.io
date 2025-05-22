6816dd4e95c2b1e53b6faa865cf2ab2a99e0b17c
Nassinger <gabrielnassynger@gmail.com>
2025-05-22 10:51:05 -0300

    update

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import { loadAllSVGs } from './svg.js';
-import { loadNavbar } from './navbar.js';
-
-loadAllSVGs();
-
-window.addEventListener('load', () => {
-    // loadNavbar();
-    document.body.removeAttribute('class');
-});
