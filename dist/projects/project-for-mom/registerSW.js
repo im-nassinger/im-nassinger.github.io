59da355ab55fd02fc4e638d12e3c76d6abf45173
Nassinger <gabrielnassynger@gmail.com>
2025-09-17 15:10:00 -0300

    random project for my mom, ignore it

--- dist/projects/project-for-mom/registerSW.js
+++ dist/projects/project-for-mom/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/projects/test/sw.js', { scope: '/projects/test/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/projects/project-for-mom/sw.js', { scope: '/projects/project-for-mom/' })})}
