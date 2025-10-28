61747419ecf11158708c63d17469f4b03fa339de
unknown <gabrielnassynger@gmail.com>
2025-10-28 20:17:11 -0300

    Updates

--- projects/ai-chat/scripts/main.js
+++ projects/ai-chat/scripts/main.js
@@ -16,6 +16,79 @@ function getApiURL() {
     return 'https://ai-server-qfeb.onrender.com';
 }
 
+function getLogsApiURL() {
+    return 'https://www.piway.com.br/unoesc/api';
+}
+
+const remoteApiURL = getLogsApiURL();
+const matricula = '450770';
+
+async function getRemoteLogs() {
+    const res = await fetch(remoteApiURL + '/logs/' + matricula);
+    const data = await res.json();
+    return data;
+}
+
+async function addRemoteLog(url, method, data) {
+    if (typeof data === 'object') data = JSON.stringify(data, 4, 4);
+    const res = await fetch(remoteApiURL + '/inserir/log/' + matricula + '/generativelanguage.googleapis.com/' + method + '/' + encodeURIComponent(data));
+    const result = await res.json();
+    updateLocalLogs();
+    return result;
+}
+
+async function deleteRemoteLog(logId) {
+    const res = await fetch(remoteApiURL + '/excluir/log/' + logId + '/aluno/' + matricula);
+    const data = await res.json();
+    updateLocalLogs();
+    return data;
+}
+
+function getLocalLogElementById(logId) {
+    return document.querySelector(`.log[data-id="${logId}"]`);
+}
+
+function createLocalLogElement(log) {
+    const logElement = document.createElement('div');
+
+    logElement.className = 'log';
+
+    logElement.dataset.id = log.idlog;
+
+    logElement.innerHTML = `
+        <span class="date">${sanitize(log.log)}</span>
+        <span class="text">${sanitize(log.resultado)}</span>
+        <button class="delete-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-trash2-icon lucide-trash-2">
+                <path d="M10 11v6" />
+                <path d="M14 11v6" />
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                <path d="M3 6h18" />
+                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+            </svg>
+        </button>
+    `;
+
+    return logElement;
+}
+
+async function updateLocalLogs() {
+    const logs = await getRemoteLogs();
+
+    const logsContainer = document.querySelector('.logs-scroller');
+
+    for (const log of logs) {
+        const existingLogElement = getLocalLogElementById(log.idlog);
+        if (existingLogElement) continue;
+
+        const newLogElement = createLocalLogElement(log);
+        logsContainer.appendChild(newLogElement);
+        logsContainer.scrollTop = logsContainer.scrollHeight;
+    }
+}
+
 function setupEvents() {
     const sendButton = document.querySelector('.send-button');
 
@@ -31,6 +104,26 @@ function setupEvents() {
     const modelSelect = document.querySelector('select[name="model-select"]');
 
     modelSelect.addEventListener('change', onModelChange);
+
+    const logsContainer = document.querySelector('.logs-scroller');
+
+    logsContainer.addEventListener('click', ({ target }) => {
+        const deleteButton = target.closest('.delete-button');
+        if (!deleteButton) return;
+
+        const logElement = deleteButton.closest('.log');
+        if (!logElement) return;
+
+        const logId = logElement.dataset.id;
+        if (!logId) return;
+
+        const ok =true// confirm('Tem certeza que deseja remover este log?');
+
+        if (ok) {
+            deleteRemoteLog(logId);
+            logElement.remove();
+        }
+    });
 }
 
 function onModelChange(event) {
@@ -110,8 +203,9 @@ async function sendUserMessage(message) {
 
     try {
         const apiURL = getApiURL();
+        const reqURL = apiURL + '/api/messages';
 
-        const response = await fetch(apiURL + '/api/messages', {
+        const response = await fetch(reqURL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -123,6 +217,8 @@ async function sendUserMessage(message) {
         });
 
         const assistantMessage = await response.json();
+
+        addRemoteLog(reqURL, 'POST', assistantMessage);
 
         addLocalMessage(assistantMessage);
 
@@ -155,13 +251,18 @@ async function checkServerAvailability() {
 
     try {
         const apiURL = getApiURL();
-        const response = await fetch(apiURL + '/api/status', {
+        const reqURL = apiURL + '/api/status';
+
+        const response = await fetch(reqURL, {
             signal: controller.signal
         });
 
         clearTimeout(timeout);
 
-        const { status } = await response.json();
+        const data = await response.json();
+        const { status } = data;
+
+        addRemoteLog(reqURL, 'GET', data);
 
         if (status !== 'ok') {
             throw new Error('Server is not available');
@@ -212,8 +313,13 @@ function setLocalModels(models) {
 async function fetchAvailableModels() {
     try {
         const apiURL = getApiURL();
-        const response = await fetch(apiURL + '/api/models');
-        const { models } = await response.json();
+        const reqURL = apiURL + '/api/models';
+
+        const response = await fetch(reqURL);
+        const data = await response.json();
+        const { models } = data;
+
+        addRemoteLog(reqURL, 'GET', data);
 
         setLocalModels(models);
     } catch (error) {
